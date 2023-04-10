a194d935fc0dd8f8a29404e968b6b5114a7a5b24
Shreyas Lutade <81281856+ShreyasLutade@users.noreply.github.com>
2023-04-10 23:30:35 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     // origin: ["http://localhost:000", "https://pinvent-app.vercel.app"],
     credentials: true,
-    origin:'http://localhost:3000',
+    origin:'https://pinvent-dh1inp4sj-shreyaslutade.vercel.app',
   })
 );
 
